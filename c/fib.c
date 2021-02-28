a10d89c19a2cd4d9ad680bee6928c23cbd930eb9
ypc2e55orj <43885603+ypc2e55orj@users.noreply.github.com>
2021-02-28 12:16:37 +0900

    remove: 余計な行

--- c/fib.c
+++ c/fib.c
@@ -13,7 +13,7 @@ long long int fib_naive(long long int n)
     return fib_naive(n - 2) + fib_naive(n - 1);
 }
 
-long long int _fib(long long int n, long long int *memo, long long int *dup)
+long long int _fib(long long int n, long long int *memo)
 {
     if (memo[n] > -1)
         return memo[n];
@@ -24,15 +24,14 @@ long long int _fib(long long int n, long long int *memo, long long int *dup)
     if (n == 1)
         return (memo[n] = 1);
 
-    memo[n - 2] = _fib(n - 2, memo, dup);
-    memo[n - 1] = _fib(n - 1, memo, dup);
+    memo[n - 2] = _fib(n - 2, memo);
+    memo[n - 1] = _fib(n - 1, memo);
 
     return (memo[n] = memo[n - 2] + memo[n - 1]);
 }
 
 long long int fib(long long int n)
 {
-    long long int dup = 0;
     long long int *memo = (long long int *)malloc((n + 1) * sizeof(long long int));
     if (memo == NULL)
         return 0;
@@ -40,7 +39,7 @@ long long int fib(long long int n)
     for (long long int i = 0; i < n + 1; i++)
         memo[i] = -1;
 
-    long long int res = _fib(n, memo, &dup);
+    long long int res = _fib(n, memo);
     free(memo);
 
     return res;
