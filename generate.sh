fd3011383eded2bd0e28e49ad2d63619177ac316
ypc2e55orj <ypc2e55orj@gmail.com>
2021-03-12 00:58:35 +0900

    add: generate

---
+++ generate.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+reserveds=(
+    ':'
+    '/'
+#    '?'
+    '#'
+    '['
+    ']'
+    '@'
+    '!'
+    '$'
+    '&'
+    "'"
+    '('
+    ')'
+#    '*'
+    '+'
+    ','
+    ';'
+    '='
+    '%'
+    ' '
+)
+IFS=$'\n'
+for i in ${reserveds[*]};
+do
+
+echo "'$i.html' generating..."
+
+cat << EOF > $i.html
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>$i.html</h1>
+        <a href="./$i.html#$i" title="permalink">$i</a>
+    </body>
+</html>
+EOF
+done
